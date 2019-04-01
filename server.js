5cfa731d97b056ebd4eea2efc2333922a8c36c9e
Marcus Bass <Bass.Marcus@icloud.com>
2019-04-01 10:28:04 -0700

    chat page renders

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ const io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
-PORT = 3000
-
 server.listen(process.env.PORT || 3000);
-console.log(`Server running on ${PORT}`)
+console.log(`Server running...`)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    //DISCONNECT
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    //SEND MESSAGE
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg: data});
+    });
+    
+});
