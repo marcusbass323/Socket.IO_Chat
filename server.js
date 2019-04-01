0af66683a753f6f5ba97c2575567eb94a2a83131
Marcus Bass <Bass.Marcus@icloud.com>
2019-04-01 15:41:10 -0700

    Chat works

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3300);
 console.log(`Server running...`)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -17,6 +17,8 @@ io.sockets.on('connection', function(socket){
 
     //DISCONNECT
     socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length);
     });
@@ -24,7 +26,20 @@ io.sockets.on('connection', function(socket){
     //SEND MESSAGE
     socket.on('send message', function(data){
         console.log(data);
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', {msg: data, user: socket.username});
     });
     
+    //NEW USER
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
+
+
 });
